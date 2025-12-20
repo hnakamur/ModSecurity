673dd78e7620ab19dac4a84d9e7d3c4f685ab705
Hiroaki Nakamura <hnakamur@gmail.com>
2025-12-20 20:55:05 +0900

    Run e2e tests using go-ftw and docker compose

---
+++ ftw/capture_run_tests.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+set -eu
+
+CRS_VERSION=${CRS_VERSION:-v4.21.0}
+
+if [ $# -ne 1 -o "$1" = "-h" -o "$1" = "--help" ]; then
+  >&2 cat <<EOF
+Usage: $0 log_dest_dir
+
+This script runs OWASP CRS regression tests using ftw and Apache httpd server.
+It also captures HTTP traffic using tcpdump.
+
+Supported optional environment variables:
+
+CRS_VERSION   Tag or version of https://github.com/coreruleset/coreruleset (default: $CRS_VERSION)
+FTW_DEBUG     Whether to enable ftw debug log.
+FTW_INCLUDE   Specify a regular expression to run only specified test cases.
+
+## Example
+
+Runs all tests:
+  $0 log
+
+Runs only tests whose ID begins with 980170 and enable debug log:
+  FTW_DEBUG=1 FTW_INCLUDE=^980170 $0 log
+EOF
+  exit 2
+fi
+
+log_dest_dir="$1"
+
+export COMPOSE_FILE=compose-capture.yml
+export COMPOSE_PROGRESS=plain
+
+docker compose build --pull --no-cache --build-arg CRS_VERSION=$CRS_VERSION
+
+mkdir -p "$log_dest_dir"
+docker compose up -d
+
+(docker compose logs -f --no-log-prefix ftw 2>&1 | tee "$log_dest_dir/run-ftw.log") &
+
+docker compose exec -d capture-ftw tcpdump -i any -U -w /home/ftw.pcap 'tcp port 8080'
+docker compose exec -d capture-crs-apache tcpdump -i any -U -w /home/crs-apache.pcap 'tcp port (8080 or 8081)'
+docker compose exec -d capture-backend tcpdump -i any -U -w /home/backend.pcap 'tcp port (8080 or 8081)'
+
+set +e
+docker compose wait ftw
+ftw_rc=$?
+
+for logfile in access.log modsec_audit.log modsec_debug.log; do
+  docker compose cp crs-apache:/var/log/apache2/$logfile "$log_dest_dir"
+done
+for svc in ftw crs-apache backend; do
+  docker compose cp "capture-$svc:/home/$svc.pcap" "$log_dest_dir"
+done
+
+docker compose down -v
+
+echo service "ftw" exited with status code $ftw_rc
+exit $ftw_rc
---
+++ ftw/tests.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# Copyright 2022 The OWASP Coraza contributors
+# SPDX-License-Identifier: Apache-2.0
+
+cd /workspace
+
+# Revisited from https://github.com/corazawaf/coraza-proxy-wasm/blob/main/ftw/tests.sh
+
+step=1
+total_steps=1
+max_retries=15 # Seconds for the server reachability timeout
+host=${1:-crs-apache}
+health_url="http://${host}:8080"
+
+# Testing if the server is up
+echo "[$step/$total_steps] Testing application reachability"
+status_code="000"
+while [[ "$status_code" -eq "000" ]]; do
+  status_code=$(curl --write-out "%{http_code}" --silent --output /dev/null "$health_url")
+  sleep 1
+  echo -ne "[Wait] Waiting for response from $health_url. Timeout: ${max_retries}s   \r"
+  let "max_retries--"
+  if [[ "$max_retries" -eq 0 ]]; then
+    echo "[Fail] Timeout waiting for response from $health_url, make sure the server is running."
+    echo "ModSecurity audit logs:" && cat /var/log/apache2/modsec_audit.log
+    exit 1
+  fi
+done
+if [[ "${status_code}" -ne "200" ]]; then
+  echo "[Fail] Unexpected response with code ${status_code} from ${health_url}, expected 200."
+  echo "ModSecurity audit logs:" && cat /var/log/apache2/modsec_audit.log
+  exit 1
+fi
+echo -e "\n[Ok] Got status code $status_code, expected 200. Ready to start."
+
+FTW_CLOUDMODE=${FTW_CLOUDMODE:-false}
+
+FTW_INCLUDE=$([ "${FTW_INCLUDE}" == "" ] && echo "" || echo "-i ${FTW_INCLUDE}")
+FTW_DEBUG=$([ "${FTW_DEBUG}" == "" ] && echo "" || echo "--debug")
+
+/ftw run -d coreruleset/tests/regression/tests $FTW_DEBUG --config ftw.yml --read-timeout=10s --max-marker-retries=50 --cloud=$FTW_CLOUDMODE $FTW_INCLUDE || exit 1
